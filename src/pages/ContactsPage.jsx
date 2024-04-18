02e68a9d2e84c55123c5d3533e6c34c7e3288e57
spalnique <lex.sydoruk@gmail.com>
2024-04-18 02:42:27 +0200

    upd

--- src/pages/ContactsPage.jsx
+++ src/pages/ContactsPage.jsx
@@ -1,7 +1,17 @@
 import ContactForm from '../components/ContactForm/ContactForm';
 import SearchBox from '../components/SearchBox/SearchBox';
 import ContactList from '../components/ContactList/ContactList';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { selectFilteredContacts } from '../redux/contacts/selectors';
+import { fetchContacts } from '../redux/contacts/operations';
 const ContactsPage = () => {
+  const dispatch = useDispatch(selectFilteredContacts);
+
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
+
   return (
     <div>
       <ContactForm />
