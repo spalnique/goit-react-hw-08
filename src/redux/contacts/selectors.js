6785999ca1889e0c08b482a05de39919082fd53c
spalnique <136997639+spalnique@users.noreply.github.com>
2024-04-24 09:04:14 +0200

    Update selectors.js

--- src/redux/contacts/selectors.js
+++ src/redux/contacts/selectors.js
@@ -2,9 +2,9 @@ import { createSelector } from '@reduxjs/toolkit';
 
 import { selectFiltersName } from '../filter/selectors';
 
-export const selectContacts = (state) => state.items;
-export const selectError = (state) => state.error;
-export const selectIsLoading = (state) => state.isLoading;
+export const selectContacts = (state) => state.contacts.items;
+export const selectError = (state) => state.contacts.error;
+export const selectIsLoading = (state) => state.contacts.isLoading;
 
 export const selectFilteredContacts = createSelector(
   [selectContacts, selectFiltersName],
